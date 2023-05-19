27be153937dc4aa366f8dfb9d44a416220e5da61
Francesco Tacinelli <francesco.tacinelli@csgalileo.net>
2023-05-19 08:06:34 +0200

    median position keypoints and line

--- yololab/yololab/dnn.py
+++ yololab/yololab/dnn.py
@@ -28,10 +28,14 @@ class Net:
 class Body:
     def __init__(self, i):
         self.xy_neck = [0,0]
+        self.xy_ilium = [0,0]
         self.i = i
+
     
     def show(self, frame):
         utils.draw_point(frame, xy=self.xy_neck, color=(255,255,0), radius=8)
+        utils.draw_point(frame, xy=self.xy_ilium, color=(255,255,0), radius=8)
+        utils.draw_segment(frame, start_point=self.xy_neck, end_point=self.xy_ilium, color=(0,0,255), thickness=3, lineType=8)
         if self.isLaying():
             utils.draw_text(frame, f"laying{self.i}", self.xy_neck, color=(255,0,255))
         else:
@@ -79,6 +83,10 @@ class NetYoloPose(Net):
                 body.xy_neck = [
                     int((keypoints[5][0] + keypoints[6][0])/2),
                     int((keypoints[5][1] + keypoints[6][1])/2),
+                ]
+                body.xy_ilium = [
+                    int((keypoints[11][0] + keypoints[12][0])/2),
+                    int((keypoints[11][1] + keypoints[12][1])/2),
                 ]
                 bodies.append(body)
             except Exception as e:
--- yololab/yololab/utils.py
+++ yololab/yololab/utils.py
@@ -9,3 +9,6 @@ def draw_text(image, text="demo", xy=(0,0), fontScale=1, color=(0,255,0)):
 
 def draw_point(image, xy=(0,0), radius=5, color=(0, 0, 255)):
     cv.circle(image, xy, radius=radius, color=color, thickness=-1)
+
+def draw_segment(image, start_point=(0,0), end_point=(0,0), color=(0,0,255), thickness=3, lineType=8):
+    cv.line(image, start_point, end_point, color=color, thickness=3, lineType=8) 
