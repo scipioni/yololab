089186394807522159111674f0082906f194d83c
frakizan <frahhsartori280819@gmail.com>
2023-06-14 08:26:27 +0200

    added more config and cli args (new width and new height)

--- yololab/yololab/reduceDataset/dataset_reducer.py
+++ yololab/yololab/reduceDataset/dataset_reducer.py
@@ -1,38 +1,40 @@
 import cv2
 
-class DatasetConverter:
-    def __init__(self, folder_path):
+class DatasetReducer:
+    def __init__(self, folder_path:str, width:int, heigth:int) -> tuple:
         self.folder_path = folder_path
+        self.width = width
+        self.height = heigth
 
-    def to_640x640(self, path):
+    def to_WxH(self, path:str):
         original_image = cv2.imread(path)
 
-        x_start = (original_image.shape[1] - 640) // 2
-        x_end = x_start + 640
-        y_start = (original_image.shape[0] - 640) // 2
-        y_end = y_start + 640
+        x_start = (original_image.shape[1] - self.width) // 2
+        x_end = x_start + self.width
+        y_start = (original_image.shape[0] - self.height) // 2
+        y_end = y_start + self.height
 
         cropped_image = original_image[y_start:y_end, x_start:x_end]
 
         cv2.imwrite(path, cropped_image)
         return (x_start, y_start, x_end, y_end)
 
-    def coordinates_converter(self, filename):
+    def coordinates_converter(self, filename:str) -> None:
         try:
             with open(filename, "r") as f:
                 flag = True
                 lines = ""
-                imageW, imageH, _, _ = self.to_640x640(filename.replace(".txt", ".jpg")) 
+                imageW, imageH, _, _ = self.to_WxH(filename.replace(".txt", ".jpg")) 
                 for line in f.readlines():
                     line = [float(x) for x in line.strip().split()]
                     # check if the coords are in a 640x640 crop
                     # flag = True if (line[1] + line[3]/2 < imageW and line[2] + line[4] < imageH) else False
                     
                     # new values
-                    xc  = (line[1] * 640) / 1920
-                    yc  = (line[2] * 640) / 1080
-                    w = (line[3] * 640) / 1920
-                    h = (line[4] * 640) / 1080
+                    xc  = (line[1] * 640) / imageW
+                    yc  = (line[2] * 640) / imageH
+                    w = (line[3] * 640) / imageW
+                    h = (line[4] * 640) / imageH
                     lines += f"{line[0]} {xc} {yc} {w} {h} \n" if flag else ""
                 
                 f.close()
