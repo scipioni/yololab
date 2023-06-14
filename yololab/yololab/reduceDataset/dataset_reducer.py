009811170defce8a740b277720d45147e25f4b22
frakizan <frahhsartori280819@gmail.com>
2023-06-14 08:45:36 +0200

    added a separated method to check if the bbox is inside the new area

--- yololab/yololab/reduceDataset/dataset_reducer.py
+++ yololab/yololab/reduceDataset/dataset_reducer.py
@@ -19,23 +19,27 @@ class DatasetReducer:
         cv2.imwrite(path, cropped_image)
         return (x_start, y_start, x_end, y_end)
 
+    def check_bbox(self, line:list, image_w:int, image_h:int):
+        return True if (line[1] + line[3]/2 < image_w + self.width/2 and
+                        line[1] - line[3]/2 < image_w - self.width/2 and
+                        line[2] + line[4]/2 < image_h + self.height/2 and
+                        line[2] - line[4]/2 < image_h - self.height/2) else False
+
+
     def coordinates_converter(self, filename:str) -> None:
         try:
             with open(filename, "r") as f:
-                flag = True
                 lines = ""
-                imageW, imageH, _, _ = self.to_WxH(filename.replace(".txt", ".jpg")) 
+                image_w, image_h, _, _ = self.to_WxH(filename.replace(".txt", ".jpg")) 
                 for line in f.readlines():
                     line = [float(x) for x in line.strip().split()]
-                    # check if the coords are in a 640x640 crop
-                    # flag = True if (line[1] + line[3]/2 < imageW and line[2] + line[4] < imageH) else False
-                    
+                     
                     # new values
-                    xc  = (line[1] * 640) / imageW
-                    yc  = (line[2] * 640) / imageH
-                    w = (line[3] * 640) / imageW
-                    h = (line[4] * 640) / imageH
-                    lines += f"{line[0]} {xc} {yc} {w} {h} \n" if flag else ""
+                    xc  = (line[1] * 640) / image_w
+                    yc  = (line[2] * 640) / image_h
+                    w = (line[3] * 640) / image_w
+                    h = (line[4] * 640) / image_h
+                    lines += f"{line[0]} {xc} {yc} {w} {h} \n" if check_bbox(line, image_w, image_h) else ""
                 
                 f.close()
 
