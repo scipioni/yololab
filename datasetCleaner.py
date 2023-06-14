ea2861059a757b303f5adef4d427971417429044
sbruccu <83294800+sbruccu@users.noreply.github.com>
2023-06-14 16:06:40 +0200

    Update datasetCleaner.py

--- datasetCleaner.py
+++ datasetCleaner.py
@@ -3,30 +3,31 @@ import argparse
 import glob
 import shutil
 
-def main(directory, imgExt):
-    def checkClass(directory):
-        path = os.path.join(directory, '*txt')
-        for file in glob.glob(os.path.join(directory, '*txt')):
-            with open(file) as f:
-                for line in f.readlines():
-                    if line[0] == '1' or line[0] == '1.0':
-                        moveLaying(file)
-            f.close()
-                    
-    def moveLaying(path):
-        path = path[:-3]
-        txtPath = path + 'txt'
-        imgPath = path + imgExt
-        if not os.path.exists('layingDataset'):
-            os.mkdir('layingDataset')
-        try:
-            shutil.copy(txtPath, os.path.join('layingDataset', os.path.basename(txtPath)))
-            shutil.copy(imgPath, os.path.join('layingDataset', os.path.basename(imgPath)))
-        except:
-            pass
+def checkClass(directory):
+    path = os.path.join(directory, '*txt')
+    for file in glob.glob(os.path.join(directory, '*txt')):
+        with open(file) as f:
+            for line in f.readlines():
+                if line[0] == '1' or line[0] == '1.0':
+                    moveLaying(file)
+        f.close()
 
-    
+def moveLaying(path):
+    path = path[:-3]
+    txtPath = path + 'txt'
+    imgPath = path + imgExt
+    if not os.path.exists('layingDataset'):
+        os.mkdir('layingDataset')
+    try:
+        shutil.copy(txtPath, os.path.join('layingDataset', os.path.basename(txtPath)))
+        shutil.copy(imgPath, os.path.join('layingDataset', os.path.basename(imgPath)))
+    except:
+        pass
+
+
+def main(directory, imgExt):        
     checkClass(directory)
+    return 
 
 
 if __name__ == "__main__":
