c1938ff789588b0978c749bb38cad6cee671f82a
Francesco Tacinelli <francesco.tacinelli@csgalileo.net>
2023-06-16 15:42:04 +0200

    Riorganizzazione progetto

--- DatasetConverter/Main.py
+++
@@ -1,6 +0,0 @@
-from config.Config import Config
-from api.DatasetConverterAPI import DatasetConverterAPI
-
-c = Config()
-api = DatasetConverterAPI()
-api.convert(c.sourceDirectory(), c.destinationDirectory(), c.inputWrapper(), c.outputWrapper())
--- yololab/yololab/reduceDataset/config.py
+++
@@ -1,5 +0,0 @@
-# Configurations
-DATASET_FOLDER = "datasets/prova"
-NEW_WIDTH = 640
-NEW_HEIGHT = 640
-
