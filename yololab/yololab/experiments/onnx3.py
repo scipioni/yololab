6c5bfaef5a21717bfadad8a276f76a5cd0f812f1
Stefano Scipioni <stefano.scipioni@csgalileo.net>
2023-06-22 20:38:34 +0200

    yolorun ultra

--- yololab/yololab/experiments/onnx3.py
+++ yololab/yololab/experiments/onnx3.py
@@ -7,8 +7,44 @@ class ObjectDetector:
         self.model_path = model_path
 
         _net = cv.dnn.readNetFromONNX(self.model_path)
-        #net = cv.dnn_DetectionModel(_net)
+        self.model = cv.dnn_DetectionModel(_net)
+        self.model.setInputSize(640, 640)
+        #self.model.setInputScale(1.0/127.5) # 255 / 2 = 127.5
+        #self.model.setInputMean((127.5, 127.5, 127.5)) # mobilenet => [-1, 1]
+        self.model.setInputSwapRB(True)
 
+    def detect_object(self, image_path):
+        img = cv.imread(image_path)
+
+
+        classIndex, confidence, bbox = self.model.detect(img, device=0, confThreshold=0.5)
+
+
+
+        # # Preprocess image (if needed) and convert to input tensor
+        # input_name = self.model.get_inputs()[0].name
+        # input_shape = self.model.get_inputs()[0].shape[2:]  # Excluding batch and channel dimensions
+        # input_size = tuple(input_shape)
+        # resized_image = cv.resize(image, input_size)
+        # input_data = np.expand_dims(resized_image.transpose(2, 0, 1), axis=0)
+
+        # # Run inference
+        # outputs = self.model.run(None, {input_name: input_data})
+
+        # # Process the output to get bounding box coordinates
+        # # Modify this section based on the output structure of your model
+        # output = outputs[0]
+        # bounding_box = output[0]
+        # x, y, w, h = bounding_box
+
+        # # Display the bounding box on the image
+        # x, y, w, h = int(x * image.shape[1]), int(y * image.shape[0]), int(w * image.shape[1]), int(h * image.shape[0])
+        # cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # # Show the image with the bounding box
+        # cv.imshow("Object Detection", image)
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()        
 
 
 
--- yolorun/yolorun/main.py
+++ yolorun/yolorun/main.py
@@ -19,11 +19,74 @@ from .yolov8.utils import blob, letterbox, path_to_list
 log = logging.getLogger(__name__)
 
 
+
+
+
+
+# async def grab(config, grabber: Grabber) -> None:
+#     device = torch.device(config.device)
+#     engine = TRTModule(config.model, device)
+#     H, W = engine.inp_info[0].shape[-2:]
+#     engine.set_desired(["num_dets", "bboxes", "scores", "labels"])
+
+#     while True:
+#         try:
+#             frame, filename = await grabber.get()
+#         except Exception as e:
+#             log.error(e)
+#             continue
+
+#         if frame is None:
+#             break
+
+#         if config.show:
+#             draw = frame.copy()
+#         frame, ratio, dwdh = letterbox(frame, (W, H))
+#         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#         tensor = blob(rgb, return_seg=False)
+#         dwdh = torch.asarray(dwdh * 2, dtype=torch.float16, device=device)
+#         tensor = torch.asarray(tensor, device=device)
+#         # inference
+#         data = engine(tensor)
+
+#         bboxes, scores, labels = det_postprocess(data)
+#         bboxes -= dwdh
+#         bboxes /= ratio
+
+#         for bbox, score, label in zip(bboxes, scores, labels):
+#             bbox = bbox.round().int().tolist()
+#             cls_id = int(label)
+#             cls = CLASSES[cls_id]
+
+#             if config.show:
+#                 color = COLORS[cls]
+#                 cv2.rectangle(draw, bbox[:2], bbox[2:], color, 2)
+#                 cv2.putText(
+#                     draw,
+#                     f"{cls}:{score:.3f}",
+#                     (bbox[0], bbox[1] - 2),
+#                     cv2.FONT_HERSHEY_SIMPLEX,
+#                     0.75,
+#                     [225, 255, 255],
+#                     thickness=2,
+#                 )
+
+#         if config.show:
+#             cv2.imshow("result", draw)
+#             cv2.waitKey(1)
+
+
 async def grab(config, grabber: Grabber) -> None:
-    device = torch.device(config.device)
-    engine = TRTModule(config.model, device)
-    H, W = engine.inp_info[0].shape[-2:]
-    engine.set_desired(["num_dets", "bboxes", "scores", "labels"])
+    from ultralytics import YOLO
+
+    model = YOLO(config.model)
+    #results = model.predict(source="folder", show=True) # Display preds. Accepts all YOLO predict arguments
+
+
+    # from ndarray
+    #im2 = cv2.imread("bus.jpg")
+
+
 
     while True:
         try:
@@ -35,41 +98,11 @@ async def grab(config, grabber: Grabber) -> None:
         if frame is None:
             break
 
-        if config.show:
-            draw = frame.copy()
-        frame, ratio, dwdh = letterbox(frame, (W, H))
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        tensor = blob(rgb, return_seg=False)
-        dwdh = torch.asarray(dwdh * 2, dtype=torch.float16, device=device)
-        tensor = torch.asarray(tensor, device=device)
-        # inference
-        data = engine(tensor)
+        #if config.show:
+        #    draw = frame.copy()
+        #results = model.predict(source=frame, show=True)  # save predictions as labels
+        results = model(frame)
 
-        bboxes, scores, labels = det_postprocess(data)
-        bboxes -= dwdh
-        bboxes /= ratio
-
-        for bbox, score, label in zip(bboxes, scores, labels):
-            bbox = bbox.round().int().tolist()
-            cls_id = int(label)
-            cls = CLASSES[cls_id]
-
-            if config.show:
-                color = COLORS[cls]
-                cv2.rectangle(draw, bbox[:2], bbox[2:], color, 2)
-                cv2.putText(
-                    draw,
-                    f"{cls}:{score:.3f}",
-                    (bbox[0], bbox[1] - 2),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.75,
-                    [225, 255, 255],
-                    thickness=2,
-                )
-
-        if config.show:
-            cv2.imshow("result", draw)
-            cv2.waitKey(1)
 
 
 # def parse_config() -> argparse.Namespace:
