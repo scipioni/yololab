d41d0f396a5a65c099bf6243bd11302f87c14a98
Stefano Scipioni <stefano.scipioni@csgalileo.net>
2023-06-23 09:37:06 +0200

    .

--- yolorun/yolorun/main.py
+++ yolorun/yolorun/main.py
@@ -21,73 +21,10 @@ log = logging.getLogger(__name__)
 from . import models
 
 
-
-
-# async def grab(config, grabber: Grabber) -> None:
-#     device = torch.device(config.device)
-#     engine = TRTModule(config.model, device)
-#     H, W = engine.inp_info[0].shape[-2:]
-#     engine.set_desired(["num_dets", "bboxes", "scores", "labels"])
-
-#     while True:
-#         try:
-#             frame, filename = await grabber.get()
-#         except Exception as e:
-#             log.error(e)
-#             continue
-
-#         if frame is None:
-#             break
-
-#         if config.show:
-#             draw = frame.copy()
-#         frame, ratio, dwdh = letterbox(frame, (W, H))
-#         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#         tensor = blob(rgb, return_seg=False)
-#         dwdh = torch.asarray(dwdh * 2, dtype=torch.float16, device=device)
-#         tensor = torch.asarray(tensor, device=device)
-#         # inference
-#         data = engine(tensor)
-
-#         bboxes, scores, labels = det_postprocess(data)
-#         bboxes -= dwdh
-#         bboxes /= ratio
-
-#         for bbox, score, label in zip(bboxes, scores, labels):
-#             bbox = bbox.round().int().tolist()
-#             cls_id = int(label)
-#             cls = CLASSES[cls_id]
-
-#             if config.show:
-#                 color = COLORS[cls]
-#                 cv2.rectangle(draw, bbox[:2], bbox[2:], color, 2)
-#                 cv2.putText(
-#                     draw,
-#                     f"{cls}:{score:.3f}",
-#                     (bbox[0], bbox[1] - 2),
-#                     cv2.FONT_HERSHEY_SIMPLEX,
-#                     0.75,
-#                     [225, 255, 255],
-#                     thickness=2,
-#                 )
-
-#         if config.show:
-#             cv2.imshow("result", draw)
-#             cv2.waitKey(1)
-
+#out = cv2.VideoWriter('output.mp4', fourcc, fps,(frame_width,frame_height),True )
 
 async def grab(config, grabber: Grabber) -> None:
-
     model = models.getModel(config)
-
-    #model = YOLO(config.model)
-    #results = model.predict(source="folder", show=True) # Display preds. Accepts all YOLO predict arguments
-
-
-    # from ndarray
-    #im2 = cv2.imread("bus.jpg")
-
-
 
     while True:
         try:
@@ -99,26 +36,9 @@ async def grab(config, grabber: Grabber) -> None:
         if frame is None:
             break
 
-        #if config.show:
-        #    draw = frame.copy()
-        ###results = model.predict(source=frame, show=True)  # save predictions as labels
-        #results = model(frame, verbose=False)
         model.predict(frame)
-
-
-
-# def parse_config() -> argparse.Namespace:
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--engine', default="models/yolov8n.engine", help='engine file')
-#     parser.add_argument('--imgs', type=str, help='Images file')
-#     parser.add_argument('--show',
-#                         action='store_true',
-#                         help='Show the detection results')
-#     parser.add_argument('--device',
-#                         default='cuda:0',
-#                         help='TensorRT infer device')
-#     config = parser.parse_config()
-#     return config
+        if config.show:
+            model.show()
 
 
 def main():
--- yolorun/yolorun/models/ultralytics.py
+++ yolorun/yolorun/models/ultralytics.py
@@ -1,3 +1,4 @@
+import cv2 as cv 
 from ultralytics import YOLO
 
 from .__init__ import Model
@@ -14,4 +15,97 @@ class ModelYolo(Model):
     def predict(self, frame):
         super().predict(frame)
 
-        self.results = self.net(self.frame, verbose=False)
+        results = self.net(self.frame, verbose=False, stream=True)
+        #self.boxes = results[0].boxes
+        self.boxes = []
+        for result in results:
+            for box in result.boxes.cpu().numpy():
+                self.boxes.append(box)
+
+    def show(self, scale=1.0):
+        self.prepare_show()
+
+        for box in self.boxes:
+            r = box.xyxy[0].astype(int)
+            cv.rectangle(self.frame_dirty, r[:2], r[2:], (255, 255, 255), 2, lineType=cv.LINE_AA)
+            cls = int(box.cls[0])
+            cv.putText(
+                self.frame_dirty,
+                f"{cls}",
+                (r[0], r[1]),
+                0,
+                0.9,
+                [85, 45, 255],
+                2,
+                lineType=cv.LINE_AA
+            )
+        #cv.waitKey(1)
+        super().show()
+
+
+# import cv2
+# import os
+# from ultralytics import YOLO
+
+# # Load the YOLOv8 model
+# model = YOLO('abc.pt')
+
+# # Open the video file
+# video_path = "path/to/video"
+# cap = cv2.VideoCapture(video_path)
+# save_path = "dir/to/save"
+# # Loop through the video frames
+# file_num = 0
+# uniques_id=set()
+# while cap.isOpened():
+#     # Read a frame from the video
+#     success, frame = cap.read()
+
+#     if success:
+#         # Run YOLOv8 inference on the frame
+#         results = model.track(frame, persist=True,conf=0.7)
+#         # print(results)
+#         if  results[0].boxes.id !=  None:
+#             boxes = results[0].boxes.xyxy.cpu().numpy().astype(int)
+    
+#             ids = results[0].boxes.id.cpu().numpy().astype(int)
+#             for box, id in zip(boxes, ids):
+#                 # Check if the id is unique
+#                 int_id =int(id)
+#                 if  int_id  not  in  unique_id:
+#                     unique_id.add(int_id)
+    
+#                     # Crop the image using the bounding box coordinates
+#                     cropped_img = frame[box[1]:box[3], box[0]:box[2]]
+    
+#                     # Save the cropped image with a unique filename
+#                     filename = f"cropped_img_{int_id}.jpg"
+#                     filepath = os.path.join(save_path, filename)
+#                     cv2.imwrite(filepath, cropped_img)
+    
+#                 # Draw the bounding box and id on the frame
+#                 cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (85, 45, 255), 2, lineType=cv2.LINE_AA)
+#                 cv2.putText(
+#                     frame,
+#                     f"Id {id}",
+#                     (box[0], box[1]),
+#                     0,
+#                     0.9,
+#                     [85, 45, 255],
+#                     2,
+#                     lineType=cv2.LINE_AA
+#                 )
+#                 cv2.imshow("Detected Frame", frame)
+#                 if cv2.waitKey(1) & 0xFF == ord("q"):
+#                     break
+
+#         # Break the loop if 'q' is pressed
+#         if cv2.waitKey(1) & 0xFF == ord("q"):
+#             break
+#     else:
+#         # Break the loop if the end of the video is reached
+#         break
+
+# # Release the video capture object and close the display window
+# cap.release()
+# cv2.destroyAllWindows()
