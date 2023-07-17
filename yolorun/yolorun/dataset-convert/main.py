6137b99b7d62042c00b3e403fcef7dd4eb07e32f
Francesco Tacinelli <francesco.tacinelli@csgalileo.net>
2023-07-17 12:13:07 +0200

    dataset autoconvert improvements (to finish)

--- yolorun/yolorun/dataset-convert/main.py
+++ yolorun/yolorun/dataset-convert/main.py
@@ -3,19 +3,9 @@ import glob
 import os
 import pathlib
 from pathlib import Path
+import multiprocessing
 
 import labelconverter as lconvert
-
-parser = argparse.ArgumentParser()
-parser.add_argument("sdir", type=str, help="input directory")
-parser.add_argument(
-    "labelformat",
-    type=str,
-    help="label output format",
-)
-args = parser.parse_args()
-args_dict = vars(args)
-
 
 class Automator:
     def __init__(self):
@@ -25,10 +15,8 @@ class Automator:
         self.labdir = []
         self.imgdir = []
         self.labsample = []
-        
 
     def dirscan(self, dir, labelext, imgext):
-
         subfolders = []
 
         for f in os.scandir(dir):
@@ -36,14 +24,20 @@ class Automator:
                 # print(f'======== {Path(f)} =======')
                 subfolders.append(f.path)
             if f.is_file():
-                if os.path.splitext(f.name)[-1].lower() in self.labelext and Path(f.path).parent not in self.labdir:
+                if (
+                    os.path.splitext(f.name)[-1].lower() in self.labelext
+                    and Path(f.path).parent not in self.labdir
+                ):
                     sample = Path(f.path)
                     labfolder = sample.parent
                     self.labsample.append(sample)
                     self.labdir.append(labfolder)
                     break
 
-                if os.path.splitext(f.name)[-1].lower() in self.imgext and Path(f.path).parent not in self.imgdir:
+                if (
+                    os.path.splitext(f.name)[-1].lower() in self.imgext
+                    and Path(f.path).parent not in self.imgdir
+                ):
                     imgfolder = Path(f.path).parent
 
                     self.imgdir.append(imgfolder)
@@ -56,19 +50,40 @@ class Automator:
         return subfolders
 
 
-    def autoConvert(self, labdir, imgdir):
 
+    def toConvert(self, labdir, imgdir, labsample):
+        print(f'imgdir {self.imgdir}, labdir {self.labdir}, labsample {self.labsample}')
         for img, lbl, sample in zip(self.imgdir, self.labdir, self.labsample):
             if img.parent == lbl.parent and sample.parent == lbl:
-                print(f'Converting {lbl.parent}')
-                importer = lconvert.Importer(sample, img)
-                converter = lconvert.Conversion(lbl, img, args.labelformat, importer.dataset)
-                exported = converter.toSelected()
+                return lbl, img, sample
 
-    def main(self):
-        scan = self.dirscan(self.sdir, self.labelext, self.imgext)
-        automatedConversion = self.autoConvert(self.labdir, self.imgdir)
+    def autoConvert(self, lbl, img, sample):
+        print(f"Converting {lbl.parent}")
+        importer = lconvert.Importer(sample, img)
+        converter = lconvert.Conversion(
+            lbl, img, args.labelformat, importer.dataset
+        )
+        exported = converter.toSelected()
 
 
-automator = Automator()
-conversion = automator.main()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sdir", type=str, help="input directory")
+    parser.add_argument(
+        "labelformat",
+        type=str,
+        help="label output format",
+    )
+
+    args = parser.parse_args()
+    args_dict = vars(args)
+
+    automator = Automator()
+    scanning = automator.dirscan(automator.sdir, automator.labelext, automator.imgext)
+    processing_args = [for i in list(automator.toConvert(automator.labdir, automator.imgdir, automator.labsample))]
+    print(f'Processing args are {processing_args}')
+
+
+    with multiprocessing.Pool(os.cpu_count()) as pool:
+        pool.starmap(automator.autoConvert, processing_args)
+    pool.close()
