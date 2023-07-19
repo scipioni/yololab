db4cdabc7ea7ecd651063c8cad542b5cf3c3383e
Stefano Scipioni <stefano.scipioni@csgalileo.net>
2023-07-19 16:37:05 +0200

    .

--- scripts/replace-first-character.sh
+++ scripts/replace-first-character.sh
@@ -4,27 +4,30 @@
 # with 81 if the first character is 0 or with 82 if the first character is 1
 
 # Get the current directory
-dir=$(pwd)
+DIR=$1
 
 # Iterate over all files in the current directory
 
-for file in *.txt; do
+
+set -x
+
+cd $DIR
+
+for FILE in *.txt; do
 
   # Check if the file is a directory
-  if [ -d "$file" ]; then
+  if [ -d "$FILE" ]; then
 
     # Recursively call the script on the directory
-    echo "Replacing first character in files in directory $file..."
-    cd "$file"
-    bash replace-first-character.sh
-    cd "$dir"
-
+    echo "Replacing first character in files in directory $FILE..."
+    #cd "$file"
+    bash replace-first-character.sh $FILE
   else
 
     # Replace the first character of each line in the file
-    echo "Replacing first character in file $file..."
-    sed -i 's/^0/82/g; s/^1/81/g' "$file"
-
+    echo "Replacing first character in file $FILE..."
+    sed -i 's/^0/81/; s/^1/82/' "$FILE"
+	break
   fi
 
 done
